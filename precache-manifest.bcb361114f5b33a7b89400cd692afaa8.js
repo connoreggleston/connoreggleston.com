12f061b5c0654433ca6c9fc00d3bdc4eeec490cf
connoreggleston <connoreggleston@outlook.com>
2019-06-24 01:02:48 +0000

    Updates

---
+++ precache-manifest.bcb361114f5b33a7b89400cd692afaa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d85da5148733d937ca124dd7a76946",
+    "url": "/connoreggleston.com/index.html"
+  },
+  {
+    "revision": "ebbbb3215b9d4351bde7",
+    "url": "/connoreggleston.com/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/connoreggleston.com/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "ebbbb3215b9d4351bde7",
+    "url": "/connoreggleston.com/static/js/main.4ed64830.chunk.js"
+  },
+  {
+    "revision": "35f21a417119d645eeb5",
+    "url": "/connoreggleston.com/static/js/runtime~main.a5ec94e6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/connoreggleston.com/precache-manifest.753c88571a434f4d4e665b74c611dda2.js"
+  "/connoreggleston.com/precache-manifest.bcb361114f5b33a7b89400cd692afaa8.js"
 );
 
 self.addEventListener('message', (event) => {
